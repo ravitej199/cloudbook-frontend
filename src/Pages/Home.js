2fee9155195c6357f346d7fb0150b4bce56a2b06
ravitej199 <75783352+ravitej199@users.noreply.github.com>
2024-07-22 20:35:40 +0530

    Made Home component responsive for mobile devices

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 function Home() {
   return (
-    <div className='flex justify-center items-center flex-col'>
-     <p className='font-semibold text-[100px] mt-32  dancing-script-custom'>Welcome to CloudBook</p>
+    <div className='flex justify-center items-center flex-col px-4'>
+     <p className='font-semibold lg:text-[100px] mt-16 md:text-8xl sm:text-5xl  dancing-script-custom'>Welcome to CloudBook</p>
 
-     <div className="buttons flex gap-7">
-       <NavLink to="/login"><button className='bg-black text-white px-3 py-2 rounded-md'>Login</button></NavLink> 
-       <NavLink to="/register"><button className='bg-black text-white px-3 py-2 rounded-md'>Register</button></NavLink>
+     <div className="buttons  flex flex-col sm:flex-row gap-4 sm:gap-7 mt-8">
+       <NavLink to="/login"><button className='bg-black text-white px-4 py-2 rounded-md w-full sm:w-auto'>Login</button></NavLink> 
+       <NavLink to="/register"><button className='bg-black text-white px-4 py-2 rounded-md w-full sm:w-auto'>Register</button></NavLink>
      </div>
     </div>
   )
